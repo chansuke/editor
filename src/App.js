067310b91ce1b643a87abdc33837ecbda02140b3
chansuke <chansuke@georepublic.de>
2017-11-25 12:51:45 +0900

    Update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import ReactDom from 'react-dom';
 import { Editor, EditorState } from 'draft-js';
 
-class Editor extends Component {
+class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { editorState: EditorState.createEmpty }
-    this.onChange = {editorState}
+     this.state = {
+      editorState: EditorState.createEmpty(),
+    };
+    this.onChange = (editorState) => this.setState({editorState});
   }
+
   render() {
     return (
-      <Editor editorState={this.state.editorState} onChange={this.onChange} />
+      <Editor
+        editorState={this.state.editorState}
+        onChange={this.onChange}
+      />
     );
   }
 }
 
-ReactDom.render(
-  <Editor />
-  document.getElementById('container')
-)
+export default App
